a77a53233645d1859aa63c357b498d4636494137
rublin <toor.ua@gmail.com>
2019-06-04 14:40:52 +0300

    boot init

---
+++ src/main/java/org/rublin/nodemonitorbot/NodeMonitorBotApplication.java
@@ -0,0 +1,13 @@
+package org.rublin.nodemonitorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeMonitorBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeMonitorBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rublin/nodemonitorbot/NodeMonitorBotApplicationTests.java
@@ -0,0 +1,16 @@
+package org.rublin.nodemonitorbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NodeMonitorBotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
