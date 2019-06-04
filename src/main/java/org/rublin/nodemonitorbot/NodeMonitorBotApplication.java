2c145165feeb2b6bfd3ed8d4382041cd76eaf0ac
rublin <toor.ua@gmail.com>
2019-06-04 15:21:18 +0300

    telegram connection works

--- src/main/java/org/rublin/nodemonitorbot/NodeMonitorBotApplication.java
+++ src/main/java/org/rublin/nodemonitorbot/NodeMonitorBotApplication.java
@@ -2,12 +2,23 @@ package org.rublin.nodemonitorbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 
+@Configuration
 @SpringBootApplication
 public class NodeMonitorBotApplication {
 
     public static void main(String[] args) {
+        ApiContextInitializer.init();
         SpringApplication.run(NodeMonitorBotApplication.class, args);
+    }
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi() {
+        return new TelegramBotsApi();
     }
 
 }
---
+++ src/main/java/org/rublin/nodemonitorbot/telegram/TelegramConnection.java
@@ -0,0 +1,68 @@
+package org.rublin.nodemonitorbot.telegram;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TelegramConnection extends TelegramLongPollingBot {
+
+    private final TelegramBotsApi telegramBotsApi;
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Value("${telegram.bot.name}")
+    private String name;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            send(update.getMessage().getText(), null, update.getMessage().getChatId());
+        }
+    }
+
+    private void send(String message, ReplyKeyboardMarkup keyboard, long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(message);
+        sendMessage.setReplyMarkup(keyboard);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Failed to send Telegram message to {}: ", chatId, e);
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @PostConstruct
+    private void init() {
+        try {
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiRequestException e) {
+            log.error("Failed to register telegram bot", e);
+        }
+    }
+}
